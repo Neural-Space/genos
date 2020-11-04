7cd0f5ba9845bd0f39376307b4a9fc7dcb9dcac1
Kushal <kushalj001@gmail.com>
2020-11-04 15:44:58 +0530

    Added instantiation logic

---
+++ src/my_package/utils.py
@@ -0,0 +1,403 @@
+"""
+Authors:
+ - Ayushman Dash <dash.ayushman.99@gmail.com>
+"""
+
+import builtins
+import copy
+
+# import hashlib
+import logging
+from dataclasses import dataclass, field
+
+# import subprocess
+# import zipfile
+# from collections import Counter
+from importlib import import_module
+
+# from pathlib import Path
+from typing import Any, Callable, Dict, List, Type, Union
+
+from omegaconf import MISSING, DictConfig, OmegaConf, _utils, read_write
+
+# from tqdm import tqdm
+# from transformers import (
+#     AutoModel,
+#     AutoTokenizer,
+#     PreTrainedModel,
+#     PreTrainedTokenizer,
+#     PreTrainedTokenizerFast,
+# )
+
+# from panini.artifact_manager import ArtifactManager
+# from panini.constants import panini_home
+# from panini.types.generic import ObjectConfig
+
+logger = logging.getLogger(__name__)
+
+
+# The following code blocks has been taken from Hydra
+# https://github.com/facebookresearch/hydra/blob/master/hydra/_internal/utils.py
+# Start Hydra block
+
+TaskFunction = Callable[[Any], Any]
+
+
+@dataclass
+# This extends Dict[str, Any] to allow for the deprecated "class" field.
+# Once support for class field removed this can stop extending Dict.
+class ObjectConfig(Dict[str, Any]):
+    """A dataclass which holds config entries to instantiate any class or function
+    Schema details here: `panini.schemas.instantiator.ObjectConfigSchema`
+    """
+
+    # class, class method or function name
+    cls: str = MISSING
+
+    # positional args if any
+    args: List = field(default_factory=list)
+
+    # parameters to pass to cls when calling it
+    params: Any = field(default_factory=dict)
+
+
+def __instantiate_class(
+    clazz: Type[Any],
+    class_config: Union[ObjectConfig, DictConfig],
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    """This function instantiates a class using a class reference
+    Args:
+        - `clazz`: (Type[Any])
+            Reference to a class
+        - `config`: (panini.types.generic.ObjectConfig)
+            An ObjectConfig or DictConfig describing what to call and what params to use
+        - `args`: (List)
+            A list of positional arguments which are passed to the constructor.
+        - `kwargs`: (Dict)
+            A dictionary of keyword arguments that are passed to the class constructor
+    Return:
+        - `object` (:obj:`object`):
+            an instance of class `clazz`
+    """
+    final_kwargs = __get_kwargs(class_config, **kwargs)
+    return clazz(*args, **final_kwargs)
+
+
+def __instantiate_class_recursive(
+    clazz: Type[Any],
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    """This function instantiates a class using a class reference
+    Args:
+        - `clazz`: (Type[Any])
+            Reference to a class
+        - `config`: (panini.types.generic.ObjectConfig)
+            An ObjectConfig or DictConfig describing what to call and what params to use
+        - `args`: (List)
+            A list of positional arguments which are passed to the constructor.
+        - `kwargs`: (Dict)
+            A dictionary of keyword arguments that are passed to the class constructor
+    Return:
+        - `object` (:obj:`object`):
+            an instance of class `clazz`
+    """
+    return clazz(*args, **kwargs)
+
+
+def __call_callable(
+    fn: Callable[..., Any],
+    config: Union[ObjectConfig, DictConfig],
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    """Calls function `fn` byy passing a given set of keyword arguments and positional arguments
+    Args:
+        - `fn`: (Callable)
+            Reference to a function
+        - `config`: (panini.types.generic.ObjectConfig)
+            An ObjectConfig or DictConfig describing what to call and what params to use
+        - `args`: (List)
+            A list of positional arguments which are passed to the constructor.
+        - `kwargs`: (Dict)
+            A dictionary of keyword arguments that are passed to the class constructor
+    Return:
+        - the return value of `fn`
+    """
+    final_kwargs = __get_kwargs(config, **kwargs)
+    return fn(*args, **final_kwargs)
+
+
+def __get_type(
+    parts: List[str], n: int, obj: type, path: str
+) -> Union[type, Callable[..., Any]]:
+    """Returns a reference to a class or a callable type given a string reference as a list.
+    E.g., ["path", "to", "some", "package"]
+       Args:
+           - `parts`: (List)
+               List of reference paths. E.g., ["path", "to", "some", "package"]
+           - `n`: (int)
+               Number of parts in the `parts`
+           - `obj`: (type)
+               The `Class` we are looking for in `parts`
+           - `path`: (str)
+               The full path to the class or callable type
+       Return:
+           - `obj_callable` or `obj_callable`:
+               instance of a class or a callable type
+    """
+    for part in parts[n:]:
+        if not hasattr(obj, part):
+            raise ValueError(
+                f"Error finding attribute ({part}) in class ({obj.__name__}): {path}"
+            )
+        obj = getattr(obj, part)
+    if isinstance(obj, type):
+        obj_type: type = obj
+        return obj_type
+    elif callable(obj):
+        obj_callable: Callable[..., Any] = obj
+        return obj_callable
+    else:
+        # dummy case
+        raise ValueError(f"Invalid type ({type(obj)}) found for {path}")
+
+
+def __locate(path: str) -> Union[type, Callable[..., Any]]:
+    """Locate an object by name or dotted path, importing as necessary.
+    This is similar to the pydoc function `locate`, except that it checks for
+    the module from the given path from back to front.
+    Args:
+        - `path`: (List)
+            string reference to some class or callable type. E.g., path.to.some.Class
+    Return:
+        - `obj_callable` or `obj_callable`:
+            instance of a class or a callable type
+    """
+
+    parts = [part for part in path.split(".") if part]
+    module = None
+    for n in reversed(range(len(parts))):
+        try:
+            module = import_module(".".join(parts[:n]))
+        except Exception as e:
+            if n == 0:
+                logger.error(f"Error loading module {path} : {e}")
+                raise e
+            continue
+        if module:
+            break
+    if module:
+        obj = module
+    else:
+        obj = builtins
+
+    return __get_type(parts, n, obj, path)
+
+
+def __get_kwargs(class_config: Union[ObjectConfig, DictConfig], **kwargs: Any) -> Any:
+    # copy config to avoid mutating it when merging with kwargs
+    config_copy = copy.deepcopy(class_config)
+
+    # Manually set parent as deepcopy does not currently handles it (https://github.com/omry/omegaconf/issues/130)
+    # noinspection PyProtectedMember
+    config_copy._set_parent(class_config._get_parent())  # type: ignore
+    class_config = config_copy
+
+    params = class_config.params if "params" in class_config else OmegaConf.create()
+    assert isinstance(
+        params, DictConfig
+    ), f"Input config params are expected to be a mapping, found {type(class_config.params).__name__}"
+    primitives = {}
+    rest = {}
+    for k, v in kwargs.items():
+        if _utils.is_primitive_type(v) or isinstance(v, (dict, list)):
+            primitives[k] = v
+        else:
+            rest[k] = v
+    final_kwargs = {}
+    with read_write(params):
+        params.merge_with(OmegaConf.create(primitives))
+
+    for k, v in params.items():
+        final_kwargs[k] = v
+
+    for k, v in rest.items():
+        final_kwargs[k] = v
+    return final_kwargs
+
+
+def __get_cls_name(config: Union[ObjectConfig, DictConfig]) -> str:
+    """A helper function to get the `cls` variable in the config
+    Args:
+        - `config`: (panini.types.generic.ObjectConfig)
+             An ObjectConfig or DictConfig describing what to call and what params to use
+    """
+    if "cls" in config:
+        return config.cls
+    else:
+        raise ValueError("Input config does not have a cls field")
+
+
+def call(config: Union[ObjectConfig, DictConfig], *args: Any, **kwargs: Any) -> Any:
+    """This function instantiates a class or a callable type
+    Args:
+       - `config`: (panini.types.generic.ObjectConfig)
+            An ObjectConfig or DictConfig describing what to call and what params to use
+       - `args`: (panini.types.generic.ObjectConfig)
+            optional positional parameters pass-through
+       - `kwargs`: (panini.types.generic.ObjectConfig)
+            optional named parameters pass-through
+    Return:
+       - the return value from the specified class or method
+    """
+    assert config is not None, "Input config is None"
+    try:
+        cls = __get_cls_name(config)
+        type_or_callable = __locate(cls)
+        if isinstance(type_or_callable, type):
+            return __instantiate_class(type_or_callable, config, *args, **kwargs)
+        else:
+            assert callable(type_or_callable)
+            return __call_callable(type_or_callable, config, *args, **kwargs)
+    except Exception as e:
+        logger.error(f"Error instantiating '{config}' : {e}")
+        raise e
+
+
+# Alias for call
+instantiate = call
+
+
+def get_class(path: str) -> type:
+    """This function returns a reference to a class given a path. E.g., path.to.some.Class
+    Args:
+       - `path`: (str)
+            The full path to the class or callable type
+    Return:
+       - a reference to the class
+    """
+    try:
+        cls = __locate(path)
+        if not isinstance(cls, type):
+            raise ValueError(f"Located non-class in {path} : {type(cls).__name__}")
+        return cls
+    except Exception as e:
+        logger.error(f"Error initializing class at {path} : {e}")
+        raise e
+
+
+def get_method(path: str) -> Callable[..., Any]:
+    """This function returns a reference to a function given a path. E.g., path.to.some.function
+    Args:
+       - `path`: (str)
+            The full path to the function
+    Return:
+       - a reference to the function
+    """
+    if not path:
+        raise ValueError("path cannot be empty. make sure to pass a correct reference.")
+    try:
+        cl = __locate(path)
+        if not callable(cl):
+            raise ValueError(f"Non callable object located : {type(cl).__name__}")
+        return cl
+    except Exception as e:
+        logger.error(f"Error getting callable at {path} : {e}")
+        raise e
+
+
+# Alias for get_method
+get_static_method = get_method
+
+
+# End Hydra block
+
+
+class RecursiveClassInstantiationError(Exception):
+    pass
+
+
+def instantiate_argument(object_config_argument: Any):
+    """This function instantiates an argument if it is a class config.
+    Otherwise it just returns the argument value.
+    This is a recursive function and instantiates arguments recursively.
+    Args:
+       - `object_config_argument`: (Any)
+            Arguments of a class as in ObjectConfig
+    Return:
+       - object_config_argument:
+            all initialized arguments
+    """
+    if (
+        isinstance(object_config_argument, dict)
+        or isinstance(object_config_argument, DictConfig)
+    ) and "cls" in object_config_argument.keys():
+        return recursive_instantiate(OmegaConf.create(object_config_argument))
+
+    if isinstance(object_config_argument, list):
+        for i, value in enumerate(object_config_argument):
+            object_config_argument[i] = instantiate_argument(value)
+    return object_config_argument
+
+
+def recursive_instantiate(
+    config: Union[ObjectConfig, DictConfig], *args: Any, **kwargs: Any
+) -> Any:
+    """This function instantiates a class recursively using an ObjectConfig.
+    Args:
+       - `config`: (panini.types.generic.ObjectConfig)
+            An ObjectConfig or DictConfig describing what to call and what params to use
+       - `args`: (panini.types.generic.ObjectConfig)
+            optional positional parameters pass-through
+       - `kwargs`: (panini.types.generic.ObjectConfig)
+            optional named parameters pass-through
+    Return:
+       - instance of the class
+    """
+    import copy
+
+    assert config is not None, "Input config is None"
+    # copy config to avoid mutating it when merging with kwargs
+    config_copy = copy.deepcopy(config)
+    config_copy._set_parent(config._get_parent())
+    config = config_copy
+
+    try:
+        # clazz = get_class(__get_cls_name(config))
+        cls = __get_cls_name(config)
+        clazz = __locate(cls)
+        params = config.params if "params" in config else OmegaConf.create()
+        if "args" in config:
+            args = config.args
+        assert isinstance(
+            params, DictConfig
+        ), "Input config params are expected to be a mapping, found {}".format(
+            type(config.params)
+        )
+        params.merge_with(OmegaConf.create(kwargs))
+
+        # Needed to avoid omegaconf.errors.UnsupportedValueType triggered by
+        # params[name] = instantiate_argument(arg)
+        params = OmegaConf.to_container(params)
+
+        args = list(args)
+        for i, arg in enumerate(args):
+            args[i] = instantiate_argument(arg)
+
+        for name, arg in params.items():
+            params[name] = instantiate_argument(arg)
+
+        if isinstance(clazz, type):
+            return __instantiate_class_recursive(clazz, *args, **params)
+        else:
+            assert callable(clazz)
+            return __call_callable(clazz, config, *args, **params)
+        # return clazz(*args, **params)
+
+    except Exception as e:
+        raise RecursiveClassInstantiationError(
+            f"Error instantiating {config['cls']} : {e}"
+        )
---
+++ tests/test_utils.py
@@ -0,0 +1,411 @@
+import copy
+
+# from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import pytest
+from my_package import utils
+from my_package.utils import ObjectConfig, RecursiveClassInstantiationError
+from omegaconf import DictConfig, OmegaConf
+
+
+class Level3:
+    def __init__(self, a_3: str, b_3: int = 10):
+        self.a_3 = a_3
+        self.b_3 = b_3
+
+    def __eq__(self, other: Any) -> Any:
+        """Overrides the default implementation"""
+        if isinstance(other, Level3):
+
+            return self.a_3 == other.a_3 and self.b_3 == other.b_3
+        return NotImplemented
+
+
+class Level2:
+    def __init__(self, a_2: Level3, b_2: int):
+        self.a_2 = a_2
+        self.b_2 = b_2
+
+    def __eq__(self, other: Any) -> Any:
+        """Overrides the default implementation"""
+        if isinstance(other, Level2):
+
+            return self.a_2 == other.a_2 and self.b_2 == other.b_2
+        return NotImplemented
+
+
+class Level1:
+    def __init__(self, a_1: Level2):
+        self.a_1 = a_1
+
+    def __eq__(self, other: Any) -> Any:
+        """Overrides the default implementation"""
+        if isinstance(other, Level1):
+
+            return self.a_1 == other.a_1
+        return NotImplemented
+
+
+# class EnumComponent:
+#     def __init__(self, language: SupportedLanguage, another_arg: Level1):
+#         self.language = language
+#         self.another_arg = another_arg
+
+#     def __eq__(self, other: Any) -> Any:
+#         """Overrides the default implementation"""
+#         if isinstance(other, EnumComponent):
+
+#             return (
+#                 self.language == other.language
+#                 and self.another_arg == other.another_arg
+#             )
+#         return NotImplemented
+
+
+# The following code block was taken from Hydra
+# https://github.com/facebookresearch/hydra/blob/master/tests/tests.test_utils.py
+# Start Hydra block
+def some_method() -> int:
+    return 42
+
+
+non_callable_object: List = []
+
+
+class Bar:
+    def __init__(self, a: Any, b: Any, c: Any, d: Any = "default_value") -> None:
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __repr__(self) -> str:
+        return f"a={self.a}, b={self.b}, c={self.c}, d={self.d}"
+
+    @staticmethod
+    def static_method() -> int:
+        return 43
+
+    def __eq__(self, other: Any) -> Any:
+        """Overrides the default implementation"""
+        if isinstance(other, Bar):
+
+            return (
+                self.a == other.a
+                and self.b == other.b
+                and self.c == other.c
+                and self.d == other.d
+            )
+        return NotImplemented
+
+    def __ne__(self, other: Any) -> Any:
+        """Overrides the default implementation (unnecessary in Python 3)"""
+        x = self.__eq__(other)
+        if x is not NotImplemented:
+            return not x
+        return NotImplemented
+
+
+class Foo:
+    def __init__(self, x: int) -> None:
+        self.x = x
+
+    def __eq__(self, other: Any) -> Any:
+        if isinstance(other, Foo):
+            return self.x == other.x
+        return False
+
+
+class Baz(Foo):
+    @classmethod
+    def class_method(self, y: int) -> Any:
+        return self(y + 1)
+
+    @staticmethod
+    def static_method(z: int) -> int:
+        return z
+
+
+class Fii:
+    def __init__(self, a: Baz = Baz(10)):
+        self.a = a
+
+    def __repr__(self) -> str:
+        return f"a={self.a}"
+
+    def __eq__(self, other: Any) -> Any:
+        """Overrides the default implementation"""
+        if isinstance(other, Fii):
+
+            return self.a == other.a
+        return NotImplemented
+
+    def __ne__(self, other: Any) -> Any:
+        """Overrides the default implementation (unnecessary in Python 3)"""
+        x = self.__eq__(other)
+        if x is not NotImplemented:
+            return not x
+        return NotImplemented
+
+
+fii = Fii()
+
+
+def fum(k: int) -> int:
+    return k + 1
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "path, expected_type", [("tests.test_utils.Bar", Bar)]
+)
+def test_get_class(path: str, expected_type: type) -> None:
+    assert utils.get_class(path) == expected_type
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "path, return_value, expected_error",
+    [
+        ("tests.test_utils.some_method", 42, None),
+        ("tests.test_utils.invalid_method", None, ValueError),
+        ("cannot.locate.this.package", None, ValueError),
+        ("tests.test_utils.non_callable_object", None, ValueError),
+    ],
+)
+def test_get_method(path: str, return_value: Any, expected_error: Exception) -> None:
+    if expected_error is not None:
+        with pytest.raises(expected_error):
+            assert utils.get_method(path)() == return_value
+    else:
+        assert utils.get_method(path)() == return_value
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "path, return_value", [("tests.test_utils.Bar.static_method", 43)]
+)
+def test_get_static_method(path: str, return_value: Any) -> None:
+    assert utils.get_static_method(path)() == return_value
+
+
+@pytest.mark.parametrize(  # type: ignore
+    "input_conf, key_to_get_config, kwargs_to_pass, expected",
+    [
+        (
+            {
+                "cls": "tests.test_utils.Bar",
+                "params": {"a": 10, "b": 20, "c": 30, "d": 40},
+            },
+            None,
+            {},
+            Bar(10, 20, 30, 40),
+        ),
+        (
+            {
+                "all_params": {
+                    "main": {
+                        "cls": "tests.test_utils.Bar",
+                        "params": {"a": 10, "b": 20, "c": "${all_params.aux.c}"},
+                    },
+                    "aux": {"c": 30},
+                }
+            },
+            "all_params.main",
+            {"d": 40},
+            Bar(10, 20, 30, 40),
+        ),
+        (
+            {"cls": "tests.test_utils.Bar", "params": {"b": 20, "c": 30}},
+            None,
+            {"a": 10, "d": 40},
+            Bar(10, 20, 30, 40),
+        ),
+        (
+            {"cls": "tests.test_utils.Bar", "params": {"b": 200, "c": "${params.b}"}},
+            None,
+            {"a": 10, "d": 40},
+            Bar(10, 200, 200, 40),
+        ),
+        # Check class and static methods
+        (
+            {"cls": "tests.test_utils.Baz.class_method", "params": {"y": 10}},
+            None,
+            {},
+            Baz(11),
+        ),
+        (
+            {"cls": "tests.test_utils.Baz.static_method", "params": {"z": 43}},
+            None,
+            {},
+            43,
+        ),
+        # Check nested types and static methods
+        ({"cls": "tests.test_utils.Fii", "params": {}}, None, {}, Fii(Baz(10))),
+        (
+            {"cls": "tests.test_utils.fii.a.class_method", "params": {"y": 10}},
+            None,
+            {},
+            Baz(11),
+        ),
+        (
+            {"cls": "tests.test_utils.fii.a.static_method", "params": {"z": 43}},
+            None,
+            {},
+            43,
+        ),
+        # Check that default value is respected
+        (
+            {"cls": "tests.test_utils.Bar", "params": {"b": 200, "c": "${params.b}"}},
+            None,
+            {"a": 10},
+            Bar(10, 200, 200, "default_value"),
+        ),
+        (
+            {"cls": "tests.test_utils.Bar", "params": {}},
+            None,
+            {"a": 10, "b": 20, "c": 30},
+            Bar(10, 20, 30, "default_value"),
+        ),
+        # call a function from a module
+        ({"cls": "tests.test_utils.fum", "params": {"k": 43}}, None, {}, 44),
+        # Check builtins
+        ({"cls": "builtins.str", "params": {"object": 43}}, None, {}, "43"),
+    ],
+)
+def test_class_instantiate(
+    input_conf: Dict[str, Any],
+    key_to_get_config: Optional[str],
+    kwargs_to_pass: Dict[str, Any],
+    expected: Any,
+) -> Any:
+    conf = OmegaConf.create(input_conf)
+    assert isinstance(conf, DictConfig)
+    if key_to_get_config is None:
+        config_to_pass = conf
+    else:
+        config_to_pass = OmegaConf.select(conf, key_to_get_config)
+    config_to_pass_copy = copy.deepcopy(config_to_pass)
+    obj = utils.instantiate(config_to_pass, **kwargs_to_pass)
+    assert obj == expected
+    # make sure config is not modified by instantiate
+    assert config_to_pass == config_to_pass_copy
+
+
+def test_class_instantiate_pass_omegaconf_node() -> Any:
+    pc = ObjectConfig()
+    # This is a bit clunky because it exposes a problem with the backport of dataclass on Python 3.6
+    # see: https://github.com/ericvsmith/dataclasses/issues/155
+    pc.cls = "tests.test_utils.Bar"
+    pc.params = {"b": 200, "c": {"x": 10, "y": "${params.b}"}}
+    conf = OmegaConf.structured(pc)
+    obj = utils.instantiate(conf, **{"a": 10, "d": Foo(99)})
+    assert obj == Bar(10, 200, {"x": 10, "y": 200}, Foo(99))
+    assert OmegaConf.is_config(obj.c)
+
+
+def test_class_warning() -> None:
+    expected = Bar(10, 20, 30, 40)
+    config = OmegaConf.structured(
+        {"cls": "tests.test_utils.Bar", "params": {"a": 10, "b": 20, "c": 30, "d": 40}}
+    )
+    assert utils.instantiate(config) == expected
+
+    config = OmegaConf.structured(
+        {"cls": "tests.test_utils.Bar", "params": {"a": 10, "b": 20, "c": 30, "d": 40}}
+    )
+    assert utils.instantiate(config) == expected
+
+
+# End Hydra block
+
+
+@pytest.mark.parametrize(
+    "input_conf, key_to_get_config, kwargs_to_pass, expected",
+    [
+        (
+            {
+                "cls": "tests.test_utils.Level1",
+                "params": {
+                    "a_1": {
+                        "cls": "tests.test_utils.Level2",
+                        "params": {
+                            "a_2": {
+                                "cls": "tests.test_utils.Level3",
+                                "params": {"a_3": "a_3", "b_3": 11},
+                            },
+                            "b_2": 42,
+                        },
+                    }
+                },
+            },
+            None,
+            {},
+            Level1(a_1=Level2(a_2=Level3(a_3="a_3", b_3=11), b_2=42)),
+        )
+    ],
+)
+def test_recursive_instantiate(
+    input_conf: Dict[str, Any],
+    key_to_get_config: Optional[str],
+    kwargs_to_pass: Dict[str, Any],
+    expected: Any,
+) -> Any:
+    conf = OmegaConf.create(input_conf)
+    assert isinstance(conf, DictConfig)
+    if key_to_get_config is None:
+        config_to_pass = conf
+    else:
+        config_to_pass = OmegaConf.select(conf, key_to_get_config)
+    config_to_pass_copy = copy.deepcopy(config_to_pass)
+    obj = utils.recursive_instantiate(config_to_pass, **kwargs_to_pass)
+    assert obj == expected
+    # make sure config is not modified by instantiate
+    assert config_to_pass == config_to_pass_copy
+
+
+@pytest.mark.parametrize(
+    "input_conf, args, expected_instantiate_exception, expected_recursive_instantiate_exception",
+    [
+        ({"cls": "some.path.that.will.fail", "params": {}}, [], Exception, Exception),
+        (
+            {"cls": "tests.test_utils.SomeLevel", "params": {}},
+            [],
+            ValueError,
+            RecursiveClassInstantiationError,
+        ),
+        (
+            {"cls": "tests.test_utils.SomeLevel", "params": {}},
+            [],
+            ValueError,
+            RecursiveClassInstantiationError,
+        ),
+        (
+            {"classsss": "tests.test_utils.SomeLevel", "params": {}},
+            [],
+            ValueError,
+            RecursiveClassInstantiationError,
+        ),
+        (
+            None,
+            [],
+            AssertionError,
+            AssertionError,
+        ),
+    ],
+)
+def test_invalid_instantiate(
+    input_conf: Dict,
+    args: List,
+    expected_instantiate_exception: Exception,
+    expected_recursive_instantiate_exception: Exception,
+):
+
+    if input_conf:
+        conf = OmegaConf.create(input_conf)
+        assert isinstance(conf, DictConfig)
+    else:
+        conf = input_conf
+    with pytest.raises(expected_instantiate_exception):
+        utils.instantiate(conf)
+
+    with pytest.raises(expected_recursive_instantiate_exception):
+        utils.recursive_instantiate(conf)

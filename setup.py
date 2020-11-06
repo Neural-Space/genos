be50f6386d3a2fff38e81fb62c989077bd9999e3
Kushal <32245327+kushalj001@users.noreply.github.com>
2020-11-07 00:19:28 +0530

    Added some required fields in setup.

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ import setuptools
 from genos.version import VERSION
 
 setuptools.setup(
+    name='genos',
     version=VERSION,
-    install_requires=[],
+    description='Instantiate objects and call functions using dictionary configs in Python using Genos.',
+    url='https://github.com/Neural-Space/genos',
+    author='Kushal Jain',
+    author_email='kushal@neuralspace.ai',
+    keywords='instantiation, objects, recursive instantiation',
+    # install_requires=[],
     python_requires='>=3.7, <4',
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src')
